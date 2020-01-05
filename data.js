46fcf8eb0ba4a1fdbf5a66f68f86a070fb769815
Hugo ESTRUCH <thejusok@gmail.com>
2020-01-06 00:27:24 +0100

    commit

--- data.js
+++ data.js
@@ -1,11 +1,21 @@
 const mission = {
   speech:[
-    "Vous voici arrivé en cours de Javascript, un nouveau projet est donné: faire un RPG. Les règles sont simples : <br> - Pas de boucle <br> - Ne pas utiliser 'var' <br> - Doit être fonctionnel <br> Le projet doit être rendu dans 3 semaines, vous décidez de:",
-    "Vous commencez a travailler maintenant mais vous n'avez pas d'inspiration pour créer une histoire potable"
+    "Bonjour, jeune aventurier",
+    "Vous commencez a travailler maintenant mais vous n'avez pas d'inspiration pour créer une histoire potable, oh non un monstre est apparue! il vous attaque il vous reste ",
+    "ygdhuaijdoaoiduyzudijokduhejiji"
   ],
   reponse:[
-    [["hey", "ok"], [1,2,3]]
+    [["Attendre le dernier moment", "zga"], [1,2]],
+    [["zszd", "hey"], [0,2]],
+    [["repo","dedz", "hduedz"], [0,1]]
+  ],
+  perso:[
+    ["Guerrier", 100, 2, 1, "Les Guerriers sont entrainer et résistant. Ils ont 100pv, 2 points de résistance et 1 points de force"],
+    ["Agile", 80, 0.5, 5, "Les Agile sont rapide et font de gros dégâts mais très fragile. Ils ont 80pv, 0.5 points de résistance et 5 points de force"],
+    ["tank", 150, 10, 0.2, "Les tanks sont très solide et résistant mais n'arrive pas a attaquer force. Ils ont 150pv, 10 points de résistances et 0.2 points de force"]
+  ],
+  combat:[
+    [],
+    ["pas beau", 20, 5, 1]
   ]
-
-
-}
+};
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // pv = point de vie
-// pr = point de volonté
+// pr = point de resistance
 // pf = force
+const perso = {"hero": "none"}
 
 const rdn = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -9,21 +10,39 @@ const rdn = (min, max) => {
 const setText = (text) => {
   document.getElementById("text").innerHTML = text;
 };
+
 const setChoice = (choices) => {
-  let choice = choices.split(";");
-  for (var i = 0; i < choice.length; i++) {
-    let button = document.createElement("button");
-    button.textContent = choice[i];
-    document.getElementById("choice").appendChild(button).setAttribute("id", i);
-  }
+    for (let i = 0; i < choices.length; i++) {
+      let button = document.createElement("button");
+      button.textContent = choices[i];
+      document.getElementById("choice").appendChild(button).setAttribute("id", i);
+      document.getElementById(i).setAttribute("onclick", `Javascript:ouJeSuis(${i})`);
+    }
 };
+
+function ouJeSuis(index) {
+  document.getElementById("text").innerHTML = "";
+  document.getElementById("choice").innerHTML = "";
+  setText(mission.speech[index]);
+  setChoice(mission.reponse[index]);
+  if (mission.combat[index].length === 4){
+    let stremon = new personnage(mission.combat[index][0], mission.combat[index][1], mission.combat[index][2], mission.combat[index][3]);
+    while (stremon.) {
+
+    }
+    perso.hero.attack(stremon);
+    stremon.attack(perso.hero);
+  }
+}
+
+//ouJeSuis(0)
 
 class personnage {
   constructor(nom, pv, pr, pf) {
     this.nom = nom;
     this.pv = pv;
-    this.pvo = pvo;
-    this.pi = pi;
+    this.pr = pr;
+    this.pf = pf;
     this.xp = 0;
     this.inv = {
       argent: 800,
@@ -38,12 +57,21 @@ class personnage {
   set setPf(pf){
     this.pf = pf;
   }
+  getter getPv
   attack(cible){
     if (this.pv > 0){
       if (cible.pv > 0) {
         let hit = (rdn(1,10) * this.pf)/cible.pr;
         console.log(`tu inflige ${hit} a ${cible.nom}`);
         cible.setPv= cible.pv - hit;
+        // Systeme d'xp
+        this.xp = rdn(2,25);
+        // Level up!
+        if (this.xp > 50){
+          this.pr *= 1.2;
+          this.pf *= 1.2;
+          this.xp = 0;
+        }
       } else {
         console.log("ta cible n'a plus assez de pv");
       }
@@ -54,8 +82,18 @@ class personnage {
   }
 }
 
-const myHero = new personnage("Pas ouf", 100, 0.5, 0.5);
-const stremon = new personnage("pas beau", 20, 5, 1);
-
-
-myHero.attack(stremon);
+document.getElementById("text").innerHTML = mission.perso[0][4]
+document.getElementById("text").innerHTML += "<br>" + mission.perso[1][4]
+document.getElementById("text").innerHTML += "<br>" + mission.perso[2][4]
+let button = document.createElement("button");
+button.textContent = mission.perso[0][0];
+document.getElementById("choice").appendChild(button).setAttribute("id", 0);
+document.getElementById(0).setAttribute("onclick", `Javascript:perso.hero = new personnage("${mission.perso[0][0]}", ${mission.perso[0][1]}, ${mission.perso[0][2]}, ${mission.perso[0][3]}); ouJeSuis(0)`);
+let button1 = document.createElement("button");
+button1.textContent = mission.perso[1][0];
+document.getElementById("choice").appendChild(button1).setAttribute("id", 1);
+document.getElementById(1).setAttribute("onclick", `Javascript:perso.hero = new personnage("${mission.perso[1][0]}", ${mission.perso[1][1]}, ${mission.perso[1][2]}, ${mission.perso[1][3]}); ouJeSuis(0)`);
+let button2 = document.createElement("button");
+button2.textContent = mission.perso[2][0];
+document.getElementById("choice").appendChild(button2).setAttribute("id", 2);
+document.getElementById(2).setAttribute("onclick", `Javascript:perso.hero = new personnage("${mission.perso[2][0]}", ${mission.perso[2][1]}, ${mission.perso[2][2]}, ${mission.perso[2][3]}); ouJeSuis(0)`);
